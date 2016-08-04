4cee36d37250f3d57296212df013a4bf9b4fa854
Franz Baumdicker <franz.baumdicker@gmx.de>
2016-08-04 11:46:45 +0200

    compute total loglikelihood

--- scripts/SF10_gain_loss.py
+++ scripts/SF10_gain_loss.py
@@ -6,6 +6,7 @@ from treetime import io
 from Bio import Phylo, AlignIO
 from SF00miscellaneous import write_json
 import sys
+from treetime import seq_utils
 
 
 def infer_gene_gain_loss(path, rates = [1.0, 1.0]):
@@ -39,6 +40,7 @@ def infer_gene_gain_loss(path, rates = [1.0, 1.0]):
     print(t.tree.get_terminals()[10])
     print(t.tree.get_terminals()[10].sequence[9375:9390])
 
+    t.tree.one_mutation = 1.0
     t.reconstruct_anc(method='ml')
     
     print(t.tree.get_terminals()[10])
@@ -174,9 +176,102 @@ def set_visible_pattern_to_ignore(tree,p = 0):
             tree.tree.clusterdict[tree.tree.patterndict[pattern][0]][1] = 0
     
     tree.tree.pattern_include = [tree.tree.clusterdict[key][1] for key in sorted(tree.tree.clusterdict.keys())]
-            
 
+
+def compute_lh(tree,verbose=0):
+    """
+    compute the likelihood for each gene presence pattern in the sequence
+    """
+
+    L = tree.tree.get_terminals()[0].sequence.shape[0]
+    n_states = tree.gtr.alphabet.shape[0]
+    if verbose > 2:
+        print ("Walking up the tree, computing likelihoods...")
+    for leaf in tree.tree.get_terminals():
+        # in any case, set the profile
+        leaf.profile = seq_utils.seq2prof(leaf.sequence, tree.gtr.profile_map)
+        leaf.lh_prefactor = np.zeros(L)
+    for node in tree.tree.get_nonterminals(order='postorder'): #leaves -> root
+        # regardless of what was before, set the profile to ones
+        node.lh_prefactor = np.zeros(L)
+        node.profile = np.ones((L, n_states)) # we will multiply it
+        for ch in node.clades:
+            ch.seq_msg_to_parent = tree.gtr.propagate_profile(ch.profile,
+                ch.branch_length,
+                rotated=False, # use unrotated
+                return_log=False) # raw prob to transfer prob up
+            node.profile *= ch.seq_msg_to_parent
+            node.lh_prefactor += ch.lh_prefactor
+        pre = node.profile.sum(axis=1) #sum over nucleotide states
+
+        node.profile = (node.profile.T/pre).T # normalize so that the sum is 1
+        node.lh_prefactor += np.log(pre) # and store log-prefactor
+    tree.tree.root.pattern_profile_lh = (np.log(tree.tree.root.profile).transpose() + tree.tree.root.lh_prefactor).transpose()
+
+def change_gtr_parameters_forgainloss(tree,pi_present,mu):
+    genepi = np.array([1.0-pi_present,pi_present])
+    genepi /= genepi.sum()
+    tree.gtr.Pi = np.diagflat(genepi)
+    # change speed
+    tree.gtr.mu = mu
+    # flow matrix
+    tree.gtr.W = np.ones((2,2))
+    np.fill_diagonal(tree.gtr.W, - ((tree.gtr.W).sum(axis=0) - 1.))
+    tree.gtr._check_fix_Q()
+    # meanwhile tree.gtr._check_fix_Q() keeps mu        
+    tree.gtr._eig()
     
+            
+def compute_totallh(tree,params,adjustcore = True,verbose = 0):
+    """
+    compute the total likelihood for all gene presence pattern in the sequence
+    be careful: this function changes the gtr model 
+    """
+    # change the relation of genegain and geneloss rate and the speed
+    pi_present = params[0]
+    mymu = params[1]
+    change_gtr_parameters_forgainloss(tree,pi_present,mymu)
+
+    # this gives the log likelihoods for each pattern
+    compute_lh(tree)
+    tree.tree.root.pattern_lh =  np.log(np.sum(np.exp(tree.tree.root.pattern_profile_lh)*np.diag(tree.gtr.Pi),axis=1))
+    # have to use nansum instead of sum since some pattern have nan likelihoods (maybe due to zero branch_length?)
+    tree.tree.root.total_llh =  np.nansum(tree.tree.root.pattern_lh * np.array(tree.tree.pattern_abundance) * np.array(tree.tree.pattern_include))
+    #adjust for pattern that should not be included
+    ll_forsumofignored = np.nansum(np.exp( tree.tree.root.pattern_lh) * np.subtract(1,tree.tree.pattern_include))
+    if verbose > 2:
+        print("adjusting nullpattern")
+    tree.tree.root.total_llh = tree.tree.root.total_llh - ( np.log(1.- ll_forsumofignored) * np.sum(np.array(tree.tree.pattern_abundance) * np.array(tree.tree.pattern_include)) )
+    return tree.tree.root.total_llh * -1.
+
+def set_seq_to_patternseq(tree):
+    for node in tree.tree.find_clades():
+        if hasattr(node, 'patternseq'):
+            node.sequence = node.patternseq
+        else:
+            delattr(node,sequence)
+
+def set_seq_to_genepresence(tree):
+    for node in tree.tree.find_clades():
+        if hasattr(node, 'genepresence'):
+            node.sequence = node.genepresence
+        else:
+            delattr(node,sequence)
+            
+def plot_ll(filename,tree,mu =1.0):
+    import matplotlib.pyplot as plt    
+    xaxis = np.linspace(0.0001, 0.999, num=500)
+    graph = [compute_totallh(tree,[x,mu]) for x in xaxis]
+    plt.plot(xaxis, graph)
+    plt.savefig(filename)
+    
+    
+def plot_ll_mu(filename,tree,pi_present =0.5):
+    import matplotlib.pyplot as plt    
+    xaxis = np.linspace(0.01, 250, num=500)
+    graph = [compute_totallh(tree,[pi_present,x]) for x in xaxis]
+    plt.plot(xaxis, graph)
+    plt.savefig(filename)
 
 if __name__=='__main__':
     species= 'Papn'

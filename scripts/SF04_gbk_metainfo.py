2885933fd78b0afee437a22144d3b245617e90d9
Wei Ding <weiding8911@gmail.com>
2016-11-16 15:04:55 +0100

    meta-info processing cleaned

--- scripts/SF04_gbk_metainfo.py
+++ scripts/SF04_gbk_metainfo.py
@@ -19,47 +19,40 @@ def gbk_To_Metainfo(path):
     for eachstrain in strainList:
         for index, record in enumerate(SeqIO.parse(open(each_gbk_path+ eachstrain+'.gbk'), "genbank")):
             for i,feature in enumerate(record.features):
+                host, datacolct, country, strainName ='unknown', 'unknown', 'unknown', 'unknown'
                 if feature.type=='source':
-                    host, datacolct, country, strainName ='', '', '', ''
                     if 'strain' in feature.qualifiers:
                         strainName= feature.qualifiers['strain'][0]
-                    else: 
-                        strainName='unknown'
                     if 'host' in feature.qualifiers:
                         host= feature.qualifiers['host'][0]
-                    else: 
-                        host='unknown'
                     if 'collection_date' in feature.qualifiers:
                         datacolct= feature.qualifiers['collection_date'][0]
                     if 'country' in feature.qualifiers:
                         country= feature.qualifiers['country'][0]
                         country= country.split(':')[0] #USA: New...
-                    else: 
-                        country='unknown'
 
                     # date processing
-                    import re, calendar
-                    datacolct= ''.join(datacolct.split('-'))
-                    dates=re.findall('\d+', datacolct);
-                    # two versions of date: 15-Seq-2011/2014-03-14
-                    if sum([str.isalpha(ic) for ic in datacolct])!=0:
-                        month_abbr=re.findall('[a-zA-Z]+', datacolct)[0]
-                        month=str(list(calendar.month_abbr).index(month_abbr))
-                        if len(datacolct)==9:
-                            if len(month)==1: month='0'+month
-                            datacolct=dates[1]+'-'+month+'-'+dates[0]
-                        else:
-                            if len(month)==1: month='0'+month
-                            datacolct=dates[0]+'-'+month+'-01'#artificial day 01
-                    elif datacolct!='':
-                        if  len(datacolct)==8:
-                            datacolct='%s-%s-%s'%(dates[0][:4], dates[0][4:6], dates[0][6:])
-                        elif len(datacolct)==6: #'2010-05'
-                            datacolct='%s-%s-01'%(dates[0][:4], dates[0][4:6])
-                        else: 
-                            datacolct=dates[0]+'-01-01'
-                    elif datacolct=='': 
-                        datacolct='unknown'
+                    if datacolct!='unknown':
+                        import re, calendar
+                        datacolct= ''.join(datacolct.split('-'))
+                        dates=re.findall('\d+', datacolct);
+                        # two versions of date: 15-Seq-2011/2014-03-14
+                        if sum([str.isalpha(ic) for ic in datacolct])!=0:
+                            month_abbr=re.findall('[a-zA-Z]+', datacolct)[0]
+                            month=str(list(calendar.month_abbr).index(month_abbr))
+                            if len(datacolct)==9:
+                                if len(month)==1: month='0'+month
+                                datacolct=dates[1]+'-'+month+'-'+dates[0]
+                            else:
+                                if len(month)==1: month='0'+month
+                                datacolct=dates[0]+'-'+month+'-01'#artificial day 01
+                        elif datacolct!='':
+                            if  len(datacolct)==8:
+                                datacolct='%s-%s-%s'%(dates[0][:4], dates[0][4:6], dates[0][6:])
+                            elif len(datacolct)==6: #'2010-05'
+                                datacolct='%s-%s-01'%(dates[0][:4], dates[0][4:6])
+                            else: 
+                                datacolct=dates[0]+'-01-01'
 
                     # just get the year
                     datacolct = datacolct.split('-')[0]

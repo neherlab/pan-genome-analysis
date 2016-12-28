4f3841ee6a6366d48b9448859390fd3592406d40
Wei Ding <weiding8911@gmail.com>
2016-12-28 17:53:55 +0100

    add params: --diamond_subject_cover;diamond_divide_conquer;subset_size;cut_branch_threshold

--- scripts/run-pipeline.py
+++ scripts/run-pipeline.py
@@ -1,13 +1,18 @@
 import argparse
 import os, sys, time, glob
 from collections import Counter
-from SF00_miscellaneous import times,load_pickle, write_pickle
+from SF00_miscellaneous import times,load_pickle, write_pickle, load_strains
 from SF02_get_acc_single_serverDown import accessionID_single
 from SF03_diamond_input import diamond_input
 from SF04_gbk_metainfo import gbk_To_Metainfo
-from SF05_diamond_orthamcl import diamond_orthamcl_cluster
+from SF05_cluster_protein import clustering_protein_sequences
+from SF05_cluster_protein_divide_conquer import clustering_divide_conquer
+#from SF05_diamond_orthamcl import diamond_orthamcl_cluster
 from SF05_2_blastRNA import RNA_cluster
+from SF06_x_geneCluster_correl_stats import cluster_align_makeTree_correl_stats 
 from SF06_geneCluster_align_makeTree import cluster_align_makeTree, postprocess_paralogs_iterative
+from SF06_0_core_diversity import estimate_core_gene_diversity
+from SF06_1_split_overclusters import postprocess_split_overclusters
 from SF06_2_unclustered_genes import postprocess_unclustered_genes
 from SF06_3_clusterRNA import RNAclusters_align_makeTree
 from SF07_core_SNP_matrix import create_core_SNP_matrix
@@ -24,25 +29,35 @@ from SF11_tree_metadata_export import json_parser
 parser = argparse.ArgumentParser(description='Pipeline for identifying core and pan-genome from NCBI RefSeq genome sequences.')
 parser.add_argument('-fn', '--folder_name', type = str, required=True, help='the absolute path for project folder ')
 parser.add_argument('-sl', '--strain_list', type = str, required=True, help='the file name for strain list (e.g.: Pa-RefSeq.txt)')
+parser.add_argument('-gbk', '--gbk_present', type = int, default = 1, help=' Using GenBank files by default. Otherwise, nucleotide/amino_acid sequence fna/faa files should be provided.')
 parser.add_argument('-st', '--steps', nargs='+', type = int, default = ['all'], help='select specific steps to run or run all steps by default ')
 parser.add_argument('-rt', '--raxml_max_time', type = int, default = 30, help='RAxML tree optimization: maximal runing time (in minutes, default: 30 min)')
 parser.add_argument('-t', '--threads', type = int, default = 1, help='number of threads')
 parser.add_argument('-bp', '--blast_file_path', type = str, default = 'none', help='the absolute path for blast result (e.g.: /path/blast.out)')
 parser.add_argument('-rp', '--roary_file_path', type = str, default = 'none', help='the absolute path for roary result (e.g.: /path/roary.out)')
 parser.add_argument('-mi', '--meta_info_file_path', type = str, default = 'none', help='the absolute path for meta_information file (e.g.: /path/meta.out)')
+parser.add_argument('-cst', '--enable_cluster_correl_stats', type = int, default = 0, help='default: calculate statistics on each cluster for correlation test')
 parser.add_argument('-np', '--disable_cluster_postprocessing', type = int, default = 0, help='default: run post-processing procedure for splitting paralogs and clustering un-clustered genes')
 parser.add_argument('-nrna', '--disable_RNA_clustering', type = int, default = 1, help='default:  disabled, not cluster rRNAs and tRNAs')
+parser.add_argument('-dme', '--diamond_evalue', type = str, default = '0.001', help='default: e-value threshold below 0.001')
 parser.add_argument('-dmt', '--diamond_max_target_seqs', type = str, default = '600', help='Diamond: the maximum number of target sequences per query to keep alignments for. Defalut: #strain * #max_duplication= 40*15= 600 ')
 parser.add_argument('-dmi', '--diamond_identity', type = str, default = '0', help='Diamond: sequence identity threshold to report an alignment. Default: empty. When applied to species with low genetic diversity: 70 could be a decent starting point. All alignments with identity below 70% will not be reported, thus also saving computational time. ')
-parser.add_argument('-dmc', '--diamond_query-cover', type = str, default = '0', help='Diamond: sequence (query) coverage threshold to report an alignment.  Default: empty. When applied to species with low genetic diversity: 70 could be a decent starting point. All alignments with less than 70% query coverage will not be reported, thus also saving computational time. ')
-#parser.add_argument('-orto', '--orthAgogue_overlap', type = int, default = 75, help='orthAgogue -o: threshold for protein pair overlap) ')
+parser.add_argument('-dmqc', '--diamond_query_cover', type = str, default = '0', help='Diamond: sequence (query) coverage threshold to report an alignment.  Default: empty. When applied to species with low genetic diversity: 70 could be a decent starting point. All alignments with less than 70% query coverage will not be reported, thus also saving computational time. ')
+parser.add_argument('-dmsc', '--diamond_subject_cover', type = str, default = '0', help='Diamond: sequence (subject) coverage threshold to report an alignment.  Default: empty. When applied to species with low genetic diversity: 70 could be a decent starting point. All alignments with less than 70%  subject coverage will not be reported, thus also saving computational time. ')
+parser.add_argument('-dmdc', '--diamond_divide_conquer', type = int, default = 0, help='Default: running diamond alignment in divide-and-conquer(DC) method is not activated; this option is designed for large dataset.')
+parser.add_argument('-dcs', '--subset_size', type = int, default = 50, help='Default:subset_size (number of strains in each subset) for divide-and-conquer(DC) method')
 parser.add_argument('-imcl', '--mcl_inflation', type = float, default = 2.0, help='MCL: inflation parameter (varying this parameter affects granularity) ')
 parser.add_argument('-bmt', '--blastn_RNA_max_target_seqs', type = str, default = '600', help='Blastn on RNAs: the maximum number of target sequences per query to keep alignments for. Defalut: #strain * #max_duplication= 40*15= 600 ')
+parser.add_argument('-cb', '--cut_branch_threshold', type = float, default = 0.3, help='branch length threshold used to decide whether to cut sub-trees for resolving over-clustering')
+parser.add_argument('-cbc', '--cut_branch_threshold_customized', type = str, default = False,\
+    help='Default: use cut-tree branch-length threshold calculated from core gene diversity; \
+        if it is set to True: use the threshold provided by user')
 parser.add_argument('-ws', '--window_size_smoothed', type = int, default = 5, help='postprocess_unclustered_genes: window_size for smoothed cluster length distribution')
 parser.add_argument('-spr', '--strain_proportion', type = float, default = 0.3, help='postprocess_unclustered_genes: strain_proportion')
 parser.add_argument('-ss', '--sigma_scale', type = int, default = 3, help='postprocess_unclustered_genes: sigma_scale')
 parser.add_argument('-kt', '--keep_temporary_file', type = str, default = True, help='default keep_temporary_file')
 parser.add_argument('-cg', '--core_genome_threshold', type = float, default = 1.0, help='percentage of strains used to decide whether a gene is core. Default: 1.0 for strictly core gene; customized instance: 0.9 for soft core genes')
+parser.add_argument('-ortha', '--orthAgogue_used', type = int, default = 0, help='Default: orthAgogue not used')
 
 params = parser.parse_args()
 path = params.folder_name
@@ -56,26 +71,8 @@ if path[:-1]!='/':
 print path
 species=strain_list_file.split('-RefSeq')[0]
 
-def load_strains():
-    """ load input strains in strain_list """
-    if os.path.isfile(path+strain_list_file):
-        with open(path+strain_list_file,'rb') as infile:
-            strain_list=[]
-            for gbk_filepath in glob.glob(path+'*gbk'):
-                gbk_filename=gbk_filepath.split('/')[-1]
-                if '-' in gbk_filename:
-                    new_gbk_filename=gbk_filename.replace('-','_')
-                    #print'mv %s %s%s'%(gbk_filepath,path,new_gbk_filename)
-                    print new_gbk_filename.split('.gbk')[0]
-                    strain_list.append(new_gbk_filename.split('.gbk')[0])
-                    os.system('mv %s %s%s'%(gbk_filepath,path,new_gbk_filename))
-                else:
-                    strain_list.append(gbk_filename.split('.gbk')[0])
-            print strain_list
-            write_pickle(path+'strain_list.cpk', strain_list )
-
 if 1 in params.steps: #step 01:
-    load_strains()
+    load_strains(path, params.gbk_present)
     print '======  step01: refSeq strain list successfully found.'
 
 ## load strain_list.cpk file and give the total number of strains
@@ -83,7 +80,8 @@ if os.path.isfile(path+'strain_list.cpk'):
     strain_list= load_pickle(path+'strain_list.cpk')
 nstrains =len([ istrain for istrain in strain_list ])
 
-if 2 in params.steps:# step02:
+## deactivated  (activation:'2' -> 2)
+if '2' in params.steps:# step02:
     print '======  starting step02: download NCBI refseq GenBank file from strain list'
     start = time.time()
     accessionID_single(path, strain_list)
@@ -93,37 +91,71 @@ if 2 in params.steps:# step02:
 if 3 in params.steps:# step03:
     print '======  starting step03: create input file for Diamond from GenBank file (.gb)'
     start = time.time()
-    diamond_input(path, strain_list, params.disable_RNA_clustering)
+    diamond_input(path, strain_list,
+        params.gbk_present, params.disable_RNA_clustering)
     print '======  time for step03: create input file for Diamond from GenBank file (.gb)'
     print times(start),'\n'
 
 if 4 in params.steps:# step04:
     print '======  starting step04: extra meta_info (country,date, host...) from GenBank file'
     start = time.time()
-    gbk_To_Metainfo(path)
+    gbk_To_Metainfo(path, params.gbk_present)
     print '======  time for step04: extra meta_info (country,date, host...) from GenBank file'
     print times(start),'\n'
 
 if 5 in params.steps:# step05:
-    print '======  starting step05: run diamond and ortha-mcl to cluster genes'
+    print '======  starting step05: cluster genes'
     start = time.time()
-    diamond_orthamcl_cluster(path, params.threads,
-        params.blast_file_path, params.roary_file_path,
-        params.diamond_max_target_seqs,
-        params.diamond_identity, params.diamond_query_cover,
-        params.mcl_inflation )
+    if params.diamond_divide_conquer==1:
+        clustering_divide_conquer( path, params.threads,
+             params.diamond_evalue, params.diamond_max_target_seqs,
+            params.diamond_identity, params.diamond_query_cover,
+            params.diamond_subject_cover, params.mcl_inflation, params.subset_size)
+    elif params.orthAgogue_used==0:
+        clustering_protein_sequences( path, params.threads,
+            params.blast_file_path, params.roary_file_path,
+            params.diamond_evalue, params.diamond_max_target_seqs,
+            params.diamond_identity, params.diamond_query_cover, params.diamond_subject_cover,
+            params.mcl_inflation
+            )
+    else:
+        diamond_orthamcl_cluster(path, params.threads,
+            params.blast_file_path, params.roary_file_path,
+            params.diamond_evalue, params.diamond_max_target_seqs,
+            params.diamond_identity, params.diamond_query_cover, params.diamond_subject_cover,
+            params.mcl_inflation )
+    ## clustering RNA when option activated
     if params.disable_RNA_clustering==0:
         RNA_cluster( path, params.threads, params.blastn_RNA_max_target_seqs, params.mcl_inflation )
-    print '======  time for step05: run diamond and ortha-mcl to cluster genes'
+    print '======  time for step05: cluster genes'
     print times(start),'\n'
 
 if 6 in params.steps:# step06:
     print '======  starting step06: align genes in geneCluster by mafft and build gene trees'
     start = time.time()
-    cluster_align_makeTree(path, params.threads, params.disable_cluster_postprocessing)
+
+    ## core gene diveristy
+    # tmp (should be uncommented)
+    #cut_branch_threshold= params.cut_branch_threshold
+    if 1: #or params.cut_branch_threshold_customized==False:
+        cut_branch_threshold=\
+            estimate_core_gene_diversity(path, params.threads, params.core_genome_threshold)
+        #cut_branch_threshold= params.cut_branch_threshold
+    #else:
+    #    cut_branch_threshold= params.cut_branch_threshold
+
+    ## align and make tree
+    if params.enable_cluster_correl_stats==1:
+        cluster_align_makeTree_correl_stats(path, params.threads, params.disable_cluster_postprocessing)
+    else:
+        cluster_align_makeTree(path, params.threads, params.disable_cluster_postprocessing)
+
+    ## with/without post-processing
     if params.disable_cluster_postprocessing==0:
+        postprocess_split_overclusters(params.threads, path, cut_branch_threshold)
         postprocess_paralogs_iterative(params.threads, path, nstrains)
-        postprocess_unclustered_genes(params.threads, path, nstrains, params.window_size_smoothed, params.strain_proportion, params.sigma_scale )
+        postprocess_unclustered_genes(params.threads, path, nstrains,\
+            params.window_size_smoothed, params.strain_proportion, params.sigma_scale )
     if params.disable_RNA_clustering==0:
         RNAclusters_align_makeTree(path, params.threads)
     print '======  time for step06: align genes in geneCluster by mafft and build gene trees'

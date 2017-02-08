290bd05feac1c26c2bf944be2e868f3565cd6d7b
Wei Ding <weiding8911@gmail.com>
2017-02-08 22:02:00 +0100

    add large_output param

--- scripts/sf_gene_presence.py
+++ scripts/sf_gene_presence.py
@@ -1,3 +1,4 @@
+import numpy as np
 from collections import defaultdict
 from sf_geneCluster_align_makeTree import load_sorted_clusters
 from sf_miscellaneous import load_pickle, write_pickle, write_in_fa, write_json
@@ -21,7 +22,7 @@ def create_genePresence(dt_strainGene, totalStrain, set_totalStrain, all_gene_na
         for ist0 in set_totalStrain-set_sharedStrain:
             dt_strainGene[ist0]='%s0'%dt_strainGene[ist0]
 
-def make_genepresence_alignment(path, enable_gain_loss):
+def make_genepresence_alignment(path, enable_gain_loss, large_output):
     '''
     loop over all gene clusters and append 0/1 to strain specific
     string used as pseudo alignment of gene presence absence
@@ -43,12 +44,21 @@ def make_genepresence_alignment(path, enable_gain_loss):
 
     with open('%s%s'%(output_path,'genePresence.aln'),'wb') as presence_outfile:
         for istkey in dt_strainGene:
-            dt_strainGene[istkey]=''.join(dt_strainGene[istkey] )
-            write_in_fa( presence_outfile, istkey, dt_strainGene[istkey] )
+            write_in_fa( presence_outfile, istkey, dt_strainGene[istkey])
     write_pickle('%s%s'%(output_path,'dt_genePresence.cpk'), dt_strainGene)
 
     if enable_gain_loss==0:
-        gene_loss_fname='%s%s'%(output_path,'geneGainLossEvent.json')
-        write_json(dt_strainGene, gene_loss_fname, indent=1)
-        geneEvents_dt={ i:0 for i in range(len(sorted_genelist)) }
-        write_pickle('%s%s'%(output_path,'dt_geneEvents.cpk'), geneEvents_dt)
+        if large_output==0:
+            gene_loss_fname='%s%s'%(output_path,'geneGainLossEvent.json')
+            write_json(dt_strainGene, gene_loss_fname, indent=1)
+            geneEvents_dt={ i:0 for i in range(len(sorted_genelist)) }
+            write_pickle('%s%s'%(output_path,'dt_geneEvents.cpk'), geneEvents_dt)
+        else:
+            ## strainID as key, presence pattern as value (converted into np.array)
+            keylist= dt_strainGene.keys(); keylist.sort()
+            strainID_keymap= {ind:k for ind, k in enumerate(keylist)} # dict(zip(keylist, range(3)))
+            presence_arr= np.array([ np.fromstring(dt_strainGene[k], np.int8)-48 for k in keylist])
+            for ind, (clusterID, gene) in enumerate(sorted_genelist):
+                pattern_dt= { strainID_keymap[strain_ind]:str(patt) for strain_ind, patt in enumerate(presence_arr[:, ind])}
+                pattern_fname='%s%s_pattern.json'%(output_path,clusterID)
+                write_json(pattern_dt, pattern_fname, indent=1)

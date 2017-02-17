459bc6aad4cc0d7b632fbbc04a2e4bf87ff0ca2a
Wei Ding <weiding8911@gmail.com>
2017-02-17 21:44:49 +0100

    organize functions into pangenome Class

--- scripts/pangenome_computation.py
+++ scripts/pangenome_computation.py
@@ -1,18 +1,33 @@
 import os, glob
 from collections import defaultdict
 from sf_miscellaneous import times, write_in_fa, load_pickle, write_pickle
+from sf_extract_sequences import extract_sequences
+from sf_extract_metadata import extract_metadata
+from sf_cluster_protein import clustering_protein
+from sf_preclustering import preclustering_protein
+from sf_cluster_protein_divide_conquer import clustering_divide_conquer
+from sf_cluster_RNA import RNA_cluster
+from sf_geneCluster_align_makeTree import cluster_align_makeTree
+from sf_core_diversity import estimate_core_gene_diversity
+from sf_split_long_branch import postprocess_split_long_branch
+from sf_split_paralogy import postprocess_paralogs_iterative
+from sf_unclustered_genes import postprocess_unclustered_genes
+from sf_RNAcluster_align_makeTree import RNAclusters_align_makeTree
+from sf_core_SNP_matrix import create_core_SNP_matrix
+from sf_core_tree_build import aln_to_Newick
+from sf_gene_presence import make_genepresence_alignment
+from sf_gain_loss import process_gain_loss
+from sf_geneCluster_json import geneCluster_to_json
+from sf_coreTree_json import json_parser
 
 class pangenome:
-    """organize and streamline pangenome analysis """
+    """streamlined pangenome analysis """
     def __init__(self, **kwargs):
         for k, v in kwargs.iteritems():
             setattr(self, k, v)
+            #self.params_dict[k]=v TODO
         self.path= '%s/'%self.path
-
-    def organize_folder(self):
-        """ create folders for pangenome analysis """
-        command_mkdir='mkdir -p '
-        folders_dict=defaultdict( str,
+        self.folders_dict=defaultdict( str,
             gbk_path='input_GenBank/',
             nucleotide_path='nucleotide_fna/',
             protein_path='protein_faa/',
@@ -20,6 +35,11 @@ class pangenome:
             RNA_path='RNA_fna/',
             cluster_seq_path='geneCluster/',
             tmp_core_seq_path='tmp_core/')
+
+    def organize_folder(self):
+        """ create folders for pangenome analysis """
+        folders_dict=self.folders_dict
+        command_mkdir='mkdir -p '
         for k,v in folders_dict.iteritems():
             folders_dict[k]='%s%s'%(self.path,v)
         for key, folder_path in folders_dict.iteritems():
@@ -27,8 +47,6 @@ class pangenome:
         self.nucleotide_path=folders_dict['nucleotide_path']
         self.protein_path=folders_dict['protein_path']
         self.clustering_path=folders_dict['clustering_path']
-        self.folders_dict=folders_dict
-        return folders_dict
 
     def specify_filepath(self):
         """ organize file paths """
@@ -79,9 +97,100 @@ class pangenome:
             os.system(command_organize_aa_input)
         write_pickle('%s%s'%(path,'strain_list.cpk'), strain_list)
         self.strain_list=strain_list
+        self.nstrains=len(strain_list)
         self.strainCollector= defaultdict()
 
-def harmonize_filename(path,glob_list):
+    def extract_gbk_sequences(self):
+        """ extract nucleotide and protein sequences from GenBank file """ 
+        extract_sequences(self.path, self.strain_list, self.folders_dict, self.gbk_present, self.disable_RNA_clustering)
+        #gene_aa_dict, gene_na_dict= extract_sequences()
+
+    def extract_gbk_metadata(self):
+        """ extract metainfo (collection date, country, etc.) from GenBank file """
+        extract_metadata(self.path, self.strain_list, self.folders_dict, self.gbk_present)
+
+    def clustering_protein_sequences(self):
+        """ clustering protein sequences"""
+        clustering_protein(self.path, self.folders_dict, self.threads, self.blast_fpath, self.roary_fpath,
+            self.orthofinder_fpath, self.other_tool_fpath, self.diamond_evalue, self.diamond_max_target_seqs,
+            self.diamond_identity, self.diamond_query_cover, self.diamond_subject_cover, self.mcl_inflation)
+
+    def finding_gene_copies(self):
+        """ running Diamond on each strain and find the duplicates """
+        preclustering_protein(self.path, self.folders_dict, self.threads,
+                self.diamond_evalue, self.diamond_max_target_seqs,
+                self.diamond_identity_subproblem, self.diamond_query_cover_subproblem,
+                self.diamond_subject_cover_subproblem)
+
+    def clustering_protein_divide_conquer(self):
+        """ applying divide and conquer algorithm to speed up all-against-all protein alignment """
+        clustering_divide_conquer(self.path, self.folders_dict, self.threads,
+             self.diamond_evalue, self.diamond_max_target_seqs,
+            self.diamond_identity, self.diamond_query_cover,
+            self.diamond_subject_cover, self.mcl_inflation, self.subset_size)
+
+    def RNA_clustering(self):
+        """clustering RNA sequences """
+        RNA_cluster(self.path, self.threads, self.blastn_RNA_max_target_seqs, self.mcl_inflation)
+
+    def estimate_raw_core_diversity(self):
+        """ computing raw core gene diversity which's refined as post-processing cutoff """
+        estimate_core_gene_diversity(self.path, self.folders_dict, self.strain_list, self.threads, self.core_genome_threshold)
+
+    def make_geneCluster_alignment_and_tree(self):
+        """ align genes in gene cluster and building gene tree """
+        cluster_align_makeTree(self.path, self.folders_dict, self.threads, self.disable_cluster_postprocessing, self.simple_tree)
+
+    def postprocessing_split_long_branch(self):
+        """ postprocessing: split long branch via refined core gene diversity """
+        postprocess_split_long_branch(self.threads, self.path, self.simple_tree, self.split_long_branch_cutoff)
+
+    def postprocessing_split_paralogs(self):
+        """ postprocessing: split paralogs"""
+        postprocess_paralogs_iterative(self.threads, self.path, self.nstrains,
+            self.simple_tree, self.paralog_branch_cutoff, self.paralog_frac_cutoff,
+            self.explore_paralog_plot)
+
+    def postprocess_merge_underclustered_genes(self):
+        """ postprocessing: integrate under_clustered genes """
+        postprocess_unclustered_genes(self.threads, self.path, self.nstrains, self.simple_tree,
+            self.window_size_smoothed, self.strain_proportion, self.sigma_scale)
+
+    def make_RNACluster_alignment_and_tree(self):
+        """ aligning RNA clusters and building RNA tree """
+        RNAclusters_align_makeTree(self.path, self.folders_dict, self.threads)
+
+    def create_SNP_alignment(self):
+        """ build pseudo-alignment based on core gene SNPs """
+        create_core_SNP_matrix(self.path, self.core_genome_threshold, self.core_gene_strain_fpath)
+
+    def build_core_tree(self):
+        """ build core tree based on SNP alignment """
+        aln_to_Newick(self.path, self.raxml_max_time, self.threads)
+
+    def compute_gene_presence_pattern(self):
+        """
+        compute gene presence/absence pattern for each gene cluster
+        (displayed on core tree)
+        """
+        make_genepresence_alignment(self.path, self.enable_gain_loss, self.large_output)
+
+    def infer_gene_gain_loss_pattern(self):
+        """
+        infer gene gain/loss pattern for each gene cluster
+        (displayed on core tree) 
+        """
+        process_gain_loss(self.path, self.large_output)
+
+    def export_geneCluster_json(self):
+        """ export gene clusters as json file which is loaded in the cluster datatable """
+        geneCluster_to_json(self.path, self.disable_RNA_clustering, self.large_output, self.raw_locus_tag)
+
+    def export_coreTree_json(self):
+        """ export core tree as json file for core tree visualization"""
+        json_parser(self.path, self.species, self.metainfo_fpath, self.large_output)
+
+def harmonize_filename(path, glob_list):
     """ force '-' to be replaced as '_' in input filename """
     for fpath in glob_list:
         gbk_fname=fpath.split('/')[-1]
--- scripts/run-pipeline.py
+++ scripts/run-pipeline.py
@@ -4,28 +4,6 @@ sys.path.append('./')
 sys.path.append('./scripts/')
 from pangenome_computation import pangenome
 from sf_miscellaneous import times, load_pickle, write_pickle, organize_folder, load_strains
-from sf_fetch_refseq import fetch_refseq
-from sf_extract_sequences import extract_sequences
-from sf_extract_metadata import extract_metadata
-from sf_cluster_protein import clustering_protein
-from sf_preclustering import preclustering_protein
-from sf_cluster_protein_divide_conquer import clustering_divide_conquer
-from sf_cluster_orthamcl import diamond_orthamcl_cluster
-from sf_cluster_RNA import RNA_cluster
-from sf_geneCluster_align_makeTree import cluster_align_makeTree
-from sf_core_diversity import estimate_core_gene_diversity
-from sf_split_paralogy import postprocess_paralogs_iterative
-from sf_split_long_branch import postprocess_split_long_branch
-from sf_unclustered_genes import postprocess_unclustered_genes
-from sf_RNAcluster_align_makeTree import RNAclusters_align_makeTree
-from sf_core_SNP_matrix import create_core_SNP_matrix
-from sf_core_tree_build import aln_to_Newick
-from sf_gene_presence import make_genepresence_alignment
-from sf_gain_loss import process_gain_loss
-from sf_geneCluster_json import geneCluster_to_json
-from sf_coreTree_json import json_parser
-#command line example
-#python run-pipeline.py -fn data/Pat3 -sl Pat3-RefSeq.txt -st 1 3 4 5 6 7 8 9 10 > Pat3.log 2>&1
 
 #python run-pipeline.py -fn data/Pmnb -sl Pat3-RefSeq.txt -st 5 -bp Pm_clustering/Pm_allclusters.tab > Pmnb-5.log 2>&1
 
@@ -57,7 +35,7 @@ parser.add_argument('-op', '--orthofinder_file_path', type = str, default = 'non
     help='the absolute path for orthofinder result (e.g.: /path/orthofinder.out)' , metavar='')
 parser.add_argument('-otp', '--other_tool_fpath', type = str, default = 'none',
     help='the absolute path for result from other orthology inference tool  (e.g.: /path/other_tool.out)' , metavar='')
-parser.add_argument('-mi', '--meta_info_file_path', type = str, default = 'none',
+parser.add_argument('-mi', '--metainfo_fpath', type = str, default = 'none',
     help='the absolute path for meta_information file (e.g.: /path/meta.out)' , metavar='')
 parser.add_argument('-dme', '--diamond_evalue', type = str, default = '0.001',
     help='default: e-value threshold below 0.001', metavar='')
@@ -79,11 +57,11 @@ parser.add_argument('-dmsc', '--diamond_subject_cover', type = str, default = '0
     When applied to species with low genetic diversity: 70 could be a decent starting point.\
     All alignments with less than 0.7 of subject coverage will not be reported, \
     thus also saving computational time. ', metavar='')
-parser.add_argument('-dmip', '--diamond_identity_precluster', type = str, default = '90',
+parser.add_argument('-dmip', '--diamond_identity_subproblem', type = str, default = '90',
     help='Diamond: sequence identity threshold to report an alignment.', metavar='')
-parser.add_argument('-dmqcp', '--diamond_query_cover_precluster', type = str, default = '90',
+parser.add_argument('-dmqcp', '--diamond_query_cover_subproblem', type = str, default = '90',
     help='Diamond: sequence (query) coverage threshold to report an alignment.', metavar='')
-parser.add_argument('-dmscp', '--diamond_subject_cover_precluster', type = str, default = '90',
+parser.add_argument('-dmscp', '--diamond_subject_cover_subproblem', type = str, default = '90',
     help='Diamond: sequence (subject) coverage threshold to report an alignment.', metavar='')
 parser.add_argument('-dmdc', '--diamond_divide_conquer', type = int, default = 0,
     help='Default: running diamond alignment in divide-and-conquer(DC) method is not activated;\
@@ -165,37 +143,38 @@ if params.steps[0]=='all':
     params.steps=range(1,12)
 
 print 'Running panX in main folder: %s'%path
-species=params.strain_list.split('-RefSeq')[0]
 #species=params.species_name TODO
 
 myPangenome=pangenome(
     path=params.folder_name,
+    species=params.strain_list.split('-RefSeq')[0],
     gbk_present=params.gbk_present,
     threads=params.threads,
-    raxml_limit=params.raxml_max_time,
+    raxml_max_time=params.raxml_max_time,
     blast_fpath=params.blast_file_path,
     roary_fpath=params.roary_file_path,
     orthofinder_fpath=params.orthofinder_file_path,
     other_tool_fpath=params.other_tool_fpath,
-    metainfo_fpath=params.meta_info_file_path,
+    metainfo_fpath=params.metainfo_fpath,
     diamond_evalue=params.diamond_evalue,
-    diamond_max_target=params.diamond_max_target_seqs,
+    diamond_max_target_seqs=params.diamond_max_target_seqs,
     diamond_identity=params.diamond_identity,
-    diamond_q_cover=params.diamond_query_cover,
-    diamond_s_cover=params.diamond_subject_cover,
-    diamond_identity_subprob=params.diamond_identity_precluster,
-    diamond_q_cover_subprob=params.diamond_query_cover_precluster,
-    diamond_s_cover_subprob=params.diamond_subject_cover_precluster,
+    diamond_query_cover=params.diamond_query_cover,
+    diamond_subject_cover=params.diamond_subject_cover,
+    diamond_identity_subproblem=params.diamond_identity_subproblem,
+    diamond_query_cover_subproblem=params.diamond_query_cover_subproblem,
+    diamond_subject_cover_subproblem=params.diamond_subject_cover_subproblem,
     diamond_dc_used=params.diamond_divide_conquer,
     diamond_dc_subsize=params.subset_size,
     mcl_inflation=params.mcl_inflation,
     blastn_max_target=params.blastn_RNA_max_target_seqs,
-    disable_postprocess=params.disable_cluster_postprocessing,
-    disable_RNA_cluster=params.disable_RNA_clustering,
+    disable_cluster_postprocessing=params.disable_cluster_postprocessing,
+    disable_RNA_clustering=params.disable_RNA_clustering,
     split_long_branch_cutoff=params.split_long_branch_cutoff,
+    explore_paralog_plot=params.explore_paralog_plot,
     paralog_frac_cutoff=params.paralog_frac_cutoff,
     paralog_branch_cutoff=params.paralog_branch_cutoff,
-    windowsize_smoothed=params.window_size_smoothed,
+    window_size_smoothed=params.window_size_smoothed,
     strain_proportion=params.strain_proportion,
     sigma_scale=params.sigma_scale,    
     core_genome_threshold=params.core_genome_threshold,
@@ -206,38 +185,32 @@ myPangenome=pangenome(
     raw_locus_tag=params.raw_locus_tag,
     keep_temporary_file=params.keep_temporary_file
     )
-folders_dict=myPangenome.organize_folder()
+
 if 1 in params.steps:#step 01:
-    #load_strains(path, params.gbk_present,folders_dict)
-    folders_dict=myPangenome.organize_folder()
+    myPangenome.organize_folder()
     myPangenome.specify_filepath()
     myPangenome.make_strain_list()
     print '======  step01: strain list successfully loaded'
-## load strain_list.cpk file and give the total number of strains
-if os.path.isfile(path+'strain_list.cpk'):
-    strain_list= load_pickle(path+'strain_list.cpk')
-nstrains =len([ istrain for istrain in strain_list ])
 
 ## deactivated  (activation:'2' -> 2)
 if '2' in params.steps:# step02:
-    print '======  starting step02: download NCBI refseq GenBank file from strain list'
+    print '======  starting step02: download NCBI RefSeq GenBank file from strain list'
     start = time.time()
     fetch_refseq(path, strain_list)
-    print '======  time for step02: download NCBI refseq GenBank file from strain list'
+    print '======  time for step02: download NCBI RefSeq GenBank file from strain list'
     print times(start),'\n'
 
 if 3 in params.steps:# step03:
     print '======  extract sequences from GenBank file'
     start = time.time()
-    gene_aa_dict, gene_na_dict= extract_sequences(path, strain_list, folders_dict, params.gbk_present,
-        params.disable_RNA_clustering)
+    myPangenome.extract_gbk_sequences()
     print '======  extract sequences from GenBank file'
     print times(start),'\n'
 
 if 4 in params.steps:# step04:
     print '======  extract metadata from GenBank file'
     start = time.time()
-    extract_metadata(path, strain_list, folders_dict, params.gbk_present)
+    myPangenome.extract_gbk_metadata()
     print '======  extract metadata from GenBank file'
     print times(start),'\n'
 
@@ -246,106 +219,79 @@ if 5 in params.steps:# step05:
     start = time.time()
     if params.diamond_divide_conquer==1:
         ## clustering with divide_and_conquer method for large dataset
-        clustering_divide_conquer(path, folders_dict, params.threads,
-             params.diamond_evalue, params.diamond_max_target_seqs,
-            params.diamond_identity, params.diamond_query_cover,
-            params.diamond_subject_cover, params.mcl_inflation, params.subset_size)
-    elif params.orthAgogue_used==0:
-        ## pre-clustering
-        if 0:
-            preclustering_protein(path, folders_dict, params.threads,
-                params.diamond_evalue, params.diamond_max_target_seqs,
-                params.diamond_identity_precluster, params.diamond_query_cover_precluster,
-                params.diamond_subject_cover_precluster)
-        ## clustering
-        if 1:
-            clustering_protein(path, folders_dict, params.threads,
-                params.blast_file_path, params.roary_file_path,
-                params.orthofinder_file_path,params.other_tool_fpath,
-                params.diamond_evalue, params.diamond_max_target_seqs,
-                params.diamond_identity, params.diamond_query_cover, params.diamond_subject_cover,
-                params.mcl_inflation
-                )
+        myPangenome.clustering_protein_divide_conquer()
     else:
-        ## test ortha+mcl
-        diamond_orthamcl_cluster(path, params.threads,
-            params.blast_file_path, params.roary_file_path,
-            params.diamond_evalue, params.diamond_max_target_seqs,
-            params.diamond_identity, params.diamond_query_cover, params.diamond_subject_cover,
-            params.mcl_inflation)
+        if 0:## pre-clustering
+            myPangenome.finding_gene_copies()
+        if 1:## clustering
+            myPangenome.clustering_protein_sequences()
     ## clustering RNA when option activated
     if params.disable_RNA_clustering==0:
-        RNA_cluster(path, params.threads, params.blastn_RNA_max_target_seqs, params.mcl_inflation)
+        myPangenome.RNA_clustering()
     print '======  cluster proteins'
     print times(start),'\n'
 
 if 6 in params.steps:# step06:
     print '======  starting step06: align genes in geneCluster by mafft and build gene trees'
     start = time.time()
-
-    ## core gene diveristy
-    if 1:
+    if 1:## core gene diveristy
         if params.split_long_branch_cutoff==0.0:
             params.split_long_branch_cutoff=\
-            estimate_core_gene_diversity(path, folders_dict, strain_list, params.threads, params.core_genome_threshold)
-    if 1:
-        ## align and make tree
-        if params.enable_cluster_correl_stats==1:
-            cluster_align_makeTree_correl_stats(path, params.threads, params.disable_cluster_postprocessing)
-        else:
-            cluster_align_makeTree(path, folders_dict, params.threads, params.disable_cluster_postprocessing, params.simple_tree)
+            myPangenome.estimate_raw_core_diversity()
+    if 1:## align and make tree
+        myPangenome.make_geneCluster_alignment_and_tree()
 
     ## with/without post-processing
     if 1 and params.disable_cluster_postprocessing==0:
         if 1:
-            postprocess_split_long_branch(params.threads, path, params.simple_tree, params.split_long_branch_cutoff)
+            myPangenome.postprocessing_split_long_branch()
         if 1:
-            if params.paralog_branch_cutoff==0.0: ## using default setting (core gene diverstiy)
+            if params.paralog_branch_cutoff==0.0:
+            ## using default setting (core gene diverstiy)
                 params.paralog_branch_cutoff=params.split_long_branch_cutoff
-            postprocess_paralogs_iterative(params.threads, path, nstrains, params.simple_tree,\
-                params.paralog_branch_cutoff, params.paralog_frac_cutoff, params.explore_paralog_plot)
+            myPangenome.postprocessing_split_paralogs()   
         if 1:
-            postprocess_unclustered_genes(params.threads, path, nstrains, params.simple_tree,\
-            params.window_size_smoothed, params.strain_proportion, params.sigma_scale )
+            myPangenome.postprocess_merge_underclustered_genes()
+
     if params.disable_RNA_clustering==0:
-        RNAclusters_align_makeTree(path, folders_dict, params.threads)
+        myPangenome.make_RNACluster_alignment_and_tree()
     print '======  time for step06: align genes in geneCluster by mafft and build gene trees'
     print times(start),'\n'
 
 if 7 in params.steps:# step07:
     print '======  starting step07: call SNPs from core genes'
     start = time.time()
-    create_core_SNP_matrix(path, params.core_genome_threshold, params.core_gene_strain_fpath)
+    myPangenome.create_SNP_alignment()
     print '======  time for step07: call SNPs from core genes'
     print times(start),'\n'
 
 if 8 in params.steps:# step08:
     print '======  starting step08: run fasttree and raxml for tree construction'
     start = time.time()
-    aln_to_Newick(path, params.raxml_max_time, params.threads)
+    myPangenome.build_core_tree()
     print '======  time for step08: run fasttree and raxml for tree construction'
     print times(start),'\n'
 
 if 9 in params.steps:# step09:
     print '======  starting step09: infer presence/absence and gain/loss patterns of all genes'
     start = time.time()
-    make_genepresence_alignment(path, params.enable_gain_loss, params.large_output)
+    myPangenome.compute_gene_presence_pattern()
     if params.enable_gain_loss==1:
-        process_gain_loss(path, params.large_output)
+        myPangenome.infer_gene_gain_loss_pattern()
     print '======  time for step09: infer presence/absence and gain/loss patterns of all genes'
     print times(start),'\n'
 
 if 10 in params.steps:# step10:
     print '======  starting step10: create json file for geneDataTable visualization'
     start = time.time()
-    geneCluster_to_json(path, params.disable_RNA_clustering, params.large_output, params.raw_locus_tag)
+    myPangenome.export_geneCluster_json()
     print '======  time for step10: create json file for geneDataTable visualization'
     print times(start),'\n'
 
 if 11 in params.steps:# step11:
     print '======  starting step11: extract json files for tree and treeDataTable visualization,etc'
     start = time.time()
-    json_parser(path, species, params.meta_info_file_path, params.large_output)
+    myPangenome.export_coreTree_json()
     print '======  time for step11: extract json files for tree and treeDataTable visualization,etc'
     print times(start),'\n'
 

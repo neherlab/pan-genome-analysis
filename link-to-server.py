a6591d98ed18534ebe3e4454c542a1628138d983
Wei Ding <weiding8911@gmail.com>
2017-04-14 20:24:25 +0200

    update file transfer script

--- link-to-server.py
+++ link-to-server.py
@@ -4,6 +4,5 @@ def link_data_to_server():
 	"""
 	Sending json files and alignment to server for data visualization
 	"""
-	os.system('rsync -avz ./data/%s/meta-dict-%s.js root@mpmtest.eb.local:/root/server-side/public/javascripts/ '%(species,species))
 	os.system('rsync -avz ./data/%s/vis/ root@mpmtest.eb.local:/root/server-side/public/dataset/%s/ '%(species,species))
 link_data_to_server()

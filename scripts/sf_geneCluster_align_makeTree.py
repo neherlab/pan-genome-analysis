25bd6a41b34d62f6079f034aa808bbec245ef954
Wei Ding <weiding8911@gmail.com>
2017-05-22 12:49:59 +0200

    mafft: --amino without --anysymbol(stop codon)

--- scripts/sf_geneCluster_align_makeTree.py
+++ scripts/sf_geneCluster_align_makeTree.py
@@ -207,7 +207,7 @@ class mpm_tree(object):
         SeqIO.write(aa_seqs.values(), tmpfname,'fasta')
 
         if alignment_tool=='mafft':
-            os.system('mafft --reorder --amino --anysymbol temp_in.fasta 1> temp_out.fasta 2> mafft.log')
+            os.system('mafft --reorder --amino temp_in.fasta 1> temp_out.fasta 2> mafft.log')
             aln_aa = AlignIO.read('temp_out.fasta', "fasta")
         elif alignment_tool=='muscle':
             from Bio.Align.Applications import MuscleCommandline

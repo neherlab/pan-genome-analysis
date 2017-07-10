c8b089c09770100a916b7d7472b1b5d39418d4e5
Wei Ding <weiding8911@gmail.com>
2017-07-10 17:06:11 +0200

    update link-to-server.py

--- link-to-server.py
+++ link-to-server.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python
 import os,sys
-species = sys.argv[1]
-def link_data_to_server():
-	"""
-	Sending json files and alignment to server for data visualization
-	"""
-	os.system('rsync -avz ./data/%s/vis/ root@mpmtest.eb.local:/root/server-side/public/dataset/%s/ '%(species,species))
-link_data_to_server()
+import argparse
+
+parser = argparse.ArgumentParser(description=\
+    'Sending panX analysis results (clusters, alignment, tree, etc) to the internal/external server for pan-genome visualization and exploration',
+    usage='./%(prog)s'+' -s E_coli -f /usr/pan-genome-visualization (-h: help)')
+parser.add_argument('-s', '--species_folder', type = str, required=True,
+    help='the folder where panX analysis pipeline runs, e.g.: E_coli (if the folder path is like this: /yourPath/pangenome_analysis/data/E_coli)', metavar='')
+parser.add_argument('-f', '--visualization_folder', type = str, required=True,
+    help='the absolute path for the visualization folder on the server, e.g.: /yourServer/pan-genome-visualization ', metavar='')
+
+params  = parser.parse_args()
+species = params.species_folder
+vis_folder = params.visualization_folder
+
+os.system('rsync -az ./data/%s/vis/ %s/public/dataset/%s/ '%(species,vis_folder,species))
+# for external sever:
+# 'rsync -az ./data/E_coli_1000/vis/ admin@xyz.abc.local:/usr/pan-genome-visualization/public/dataset/E_coli_1000/

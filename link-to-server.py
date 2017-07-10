badf71af28883d370247b81d2efdd2d7c8a43c56
Wei Ding <weiding8911@gmail.com>
2017-07-10 17:10:11 +0200

    update link-to-server.py

--- link-to-server.py
+++ link-to-server.py
@@ -4,10 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description=\
     'Sending panX analysis results (clusters, alignment, tree, etc) to the internal/external server for pan-genome visualization and exploration',
-    usage='./%(prog)s'+' -s E_coli -f /usr/pan-genome-visualization (-h: help)')
+    usage='./%(prog)s'+' -s E_coli -v /usr/pan-genome-visualization (-h: help)')
 parser.add_argument('-s', '--species_folder', type = str, required=True,
     help='the folder where panX analysis pipeline runs, e.g.: E_coli (if the folder path is like this: /yourPath/pangenome_analysis/data/E_coli)', metavar='')
-parser.add_argument('-f', '--visualization_folder', type = str, required=True,
+parser.add_argument('-v', '--visualization_folder', type = str, required=True,
     help='the absolute path for the visualization folder on the server, e.g.: /yourServer/pan-genome-visualization ', metavar='')
 
 params  = parser.parse_args()

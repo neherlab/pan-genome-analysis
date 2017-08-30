864cdd78669f71de47bb4fec8a577be7155ce4dc
Wei Ding <weiding8911@gmail.com>
2017-08-30 16:40:53 +0200

    update file transfer

--- scripts/sf_coreTree_json.py
+++ scripts/sf_coreTree_json.py
@@ -263,14 +263,14 @@ def json_parser( path, folders_dict, fpaths_dict, meta_info_file_path,
 
     os.chdir(output_path)
     os.system('mv coreGenomeTree.json strainMetainfo.json '+main_data_path+'metaConfiguration.js '+vis_json_path)
-    os.system('mv GC*_tree.json GC*.nwk '+vis_cluster_path)
-    os.system('mv GC*_aln*.fa '+vis_cluster_path)
+    os.system('mv *_tree.json *.nwk '+vis_cluster_path)
+    os.system('mv *_aln*.fa '+vis_cluster_path)
     os.system('cp strain_tree.nwk '+vis_json_path+'/strain_tree.nwk')
-    os.system('mv GC*patterns.json '+vis_cluster_path)
+    os.system('mv *patterns.json '+vis_cluster_path)
     os.system('mv '+clustering_path+'allclusters_final.tsv'+' '+main_data_path)
 
     ## gzip aln files
-    os.system('gzip -f '+vis_cluster_path+'GC*_aln*.fa' )
+    os.system('gzip -f '+vis_cluster_path+'*_aln*.fa' )
     if clean_temporary_files:
         # clean up record folders
         os.chdir(main_data_path);
